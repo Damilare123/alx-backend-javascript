3fd735596f239195d0f5815dc968ea3f94d43d17
John Damilare <dare.john18@gmail.com>
2022-12-22 12:39:39 +0100

    Add files via upload

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,10 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(empList) {
+      return Object.keys(empList).length;
     },
   };
 }
